458af009ce46359e790ac8a9cb10d1216eb6cc05
DeepKungChannel <ppoumppoum@gmail.com>
2021-07-30 21:19:51 +0700

    Add Q6.py

---
+++ Q6.py
@@ -0,0 +1,9 @@
+
+
+print("\n\n\n")
+print("Make a flip program if return 1 display หัว if return 0 display ก้อย")
+
+print('''Example:
+    def flip():
+        write a random numbee algorithm''')
+print("\n\n\n")
