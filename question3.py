6320baf724c6e9776ab4e380fa5a316285f93a36
DeepKungChannel <ppoumppoum@gmail.com>
2021-07-30 17:47:34 +0700

    Fix question3.py

--- question3.py
+++ question3.py
@@ -1,2 +1,2 @@
-print("Make a login system | if password = 1132 show welcome dk if not show incorrect password")
-# Make a login system | if password = 1132 show welcome dk if not show incorrect password
+print("Make a login system | password = input() if password = 1132 show welcome dk if not show incorrect password")
+# Make a login system | password = input() if password = 1132 show welcome dk if not show incorrect password
