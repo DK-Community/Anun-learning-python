03cc2d350d96067dc5e052934e1676dac691f668
deepkung1904 <ppoumppoum@gmail.com>
2021-07-30 15:42:53 +0700

    ให้โจทย์ที่ 1

--- learn1.py
+++ learn1.py
@@ -1,1 +1,10 @@
-print("hello anun")
+def calculate_circle_area(radius):
+    pass # change it to your code
+
+
+if __name__ == '__main__':
+    calculate_circle_area(12)
+
+
+# Answer here
+# output จะเป็นอะไร
