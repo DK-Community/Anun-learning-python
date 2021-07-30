3374ac71aa4dc26b0595e70596a7c5df5dc133bc
DeepKungChannel <ppoumppoum@gmail.com>
2021-07-30 17:28:22 +0700

    Give Question 3

---
+++ question3.py
@@ -0,0 +1,2 @@
+print("Make a login system | if password = 1132 show welcome dk if not show incorrect password")
+# Make a login system | if password = 1132 show welcome dk if not show incorrect password
