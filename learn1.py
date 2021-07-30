6705f979297c2fe957058517a9be0f1a1ed3a89d
AlexSan12 <anun.alex12@gmail.com>
2021-07-30 16:10:50 +0700

    Already Answer (1)

--- learn1.py
+++ learn1.py
@@ -1,10 +1,13 @@
+import math
 def calculate_circle_area(radius):
-    pass # change it to your code
+    area = math.pi * (radius**2)
+    print(area)
 
 
-if __name__ == '__main__':
+if __name__  == '__main__':
     calculate_circle_area(12)
 
 
 # Answer here
 # output จะเป็นอะไร
+# 452.39
