40b6fef76803594e0406b4f1418da634e3afa5d5
DeepKungChannel <ppoumppoum@gmail.com>
2021-07-30 20:06:28 +0700

    Fix spelling in question5.py

--- question5.py
+++ question5.py
@@ -1,4 +1,4 @@
-print("make a progrom that take a input in a list")
+print("make a program that take a input in a list")
 print('''
         example input : 1 3 4 5
         example output : [1,3,4,5]
