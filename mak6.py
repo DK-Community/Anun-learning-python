455d4fa2819704655e228c55055ec0c98458ba98
DeepKungChannel <ppoumppoum@gmail.com>
2021-07-31 18:32:15 +0700

    wrong mak6.py take it into function

--- mak6.py
+++ mak6.py
@@ -6,3 +6,18 @@ if c == 1:
 else:
     print("ก้อย")
 
+
+"""
+make it in to function like this
+
+def flip():
+    c = random.randint(0,1)
+    if c == 1:
+        print("หัว")
+    else:
+        print("ก้อย")
+
+flip()
+
+
+"""
