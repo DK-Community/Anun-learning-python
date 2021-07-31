f89743d494d4ec4f381c811dd6ef7c0141e36fb6
DeepKungChannel <ppoumppoum@gmail.com>
2021-07-31 16:05:01 +0700

    Wrong Alex6.py the question need filp function

--- Alex6.py
+++ Alex6.py
@@ -3,7 +3,10 @@ import random
 หัว = 0
 ก้อย = 0
 
-flip = int()
+# turn everythings into function filp and call it
+
+
+flip = int() # delete this line
 result = random.randint(0,1)
 if result == 1:
     print("หัว")
