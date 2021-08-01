564c0ebdceab61f81aaf43b34a250b459ccb07eb
DeepKungChannel <ppoumppoum@gmail.com>
2021-08-01 07:12:41 +0700

    Add Q7.py in Q7 folder

---
+++ Q7/Q7.py
@@ -0,0 +1,18 @@
+print("\n"*3)
+print("Make a sum program use function to return a result..")
+print("""Example:
+    input : 3 4 20 5
+    output: 32
+
+    input : 2 5 4
+    output : 11
+
+Example coding:
+def sum(list):
+    #writing your program here
+    return result
+
+print(sum(list))
+
+""")
+print("\n"*3)
